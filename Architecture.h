c5e712d923e7597dc4e7a081cf302ec730cbb96d
AraZobayan <EnglishMan24@126.com>
2025-12-26 22:26:40 +0800

    张嘉恒
    新的建筑图片，头文件中定义建筑

--- Architecture.h
+++ Architecture.h
@@ -6,17 +6,26 @@
 enum class BuildingType {
     COMMAND_CENTER,
     GOLD_MINE,
-    ELIXIR_COLLECTOR
+    ELIXIR_COLLECTOR,
+    VAULT,
+    BARRACKS,
+    WALL,
+    ARCHER_TOWER,
+    CANNON,
 };
 
 enum class ResourceType {
     GOLD,
     ELIXIR,
-    DARK_ELIXIR，
-
 };
-static const int GOLD_MINE_CONSUME = 100;       // 金矿建造消耗
-static const int ELIXIR_COLLECTOR_CONSUME = 80; // 圣水收集器建造消耗
+static const int GOLD_MINE_CONSUME = 100;       // 金矿建造消耗（√）
+static const int ELIXIR_COLLECTOR_CONSUME = 80; // 圣水收集器建造消耗（√）
+static const int BARRACKS_CONSUME = 150;        // 军营建造消耗（√）
+static const int WALL_CONSUME = 10;             // 城墙建造消耗（√）
+static const int ARCHER_TOWER_CONSUME = 120;    // 弓箭塔建造消耗（√）
+static const int CANNON_CONSUME = 100;          // 加农炮建造消耗（√）
+static const int VAULT_CONSUME = 100;           // 金库建造消耗（√）
+static const int ELIXIR_FONT_CONSUME = 80;      // 圣水罐建造消耗（已找到资源图片）
 class Architecture : public cocos2d::Sprite
 {
 public:
